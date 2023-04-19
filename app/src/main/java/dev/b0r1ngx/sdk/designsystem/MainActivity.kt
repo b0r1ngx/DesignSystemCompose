3688f3ea8a089d5fb0df886d7e61d4388f0b4313
Kirill I. A <ghztrikz1337@gmail.com>
2023-04-19 13:52:55 +0300

    Initial commit - standard android compose project 4/19/23

---
+++ app/src/main/java/dev/b0r1ngx/sdk/designsystem/MainActivity.kt
@@ -0,0 +1,46 @@
+package dev.b0r1ngx.sdk.designsystem
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.b0r1ngx.sdk.designsystem.ui.theme.DesignSystemTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DesignSystemTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DesignSystemTheme {
+        Greeting("Android")
+    }
+}
